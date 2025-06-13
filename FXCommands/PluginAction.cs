0b3232580cdd0e3041841047ca67ab9d480eeb84
Josh Bowden <git@josh.tf>
2025-06-13 16:50:41 +1000

    refactor: streamline PluginAction class and improve state management

--- FXCommands/PluginAction.cs
+++ FXCommands/PluginAction.cs
@@ -1,20 +1,20 @@
-﻿using BarRaider.SdTools;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Threading.Tasks;
+using BarRaider.SdTools;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace FXCommands
 {
     [PluginActionId("tf.josh.fxcommands")]
     public class PluginAction : PluginBase
     {
-
-        readonly static int MAXSTATES = 5;
-        private static bool firstPluginLoad = true; 
+        private const int MAXSTATES = 5;
         private int currentState = 0;
-        
+        private PluginSettings settings;
+        private ConnectionManager connectionManager;
+
         private CommandAction CurrentCommandAction => settings.commands[currentState];
 
         private class PluginSettings
@@ -22,230 +22,108 @@ namespace FXCommands
             [JsonProperty(PropertyName = "currentState")]
             public int? StoredState { get; set; } = null;
 
-            PluginSettings()
+            [JsonProperty(PropertyName = "desiredStates")]
+            public int DesiredStates { get; set; }
+
+            [JsonProperty(PropertyName = "commands")]
+            public List<CommandAction> commands { get; set; }
+
+            public PluginSettings()
             {
                 commands = new List<CommandAction>(MAXSTATES);
                 for (int i = 0; i < MAXSTATES; i++)
-                {
                     commands.Add(new CommandAction());
-                }
+                DesiredStates = MAXSTATES;
             }
 
-            public static PluginSettings CreateDefaultSettings()
-            {
-                PluginSettings instance = new PluginSettings();
+            public static PluginSettings CreateDefaultSettings() => new PluginSettings();
 
-                instance.commands = new List<CommandAction>(MAXSTATES);
-                for (int i = 0; i < MAXSTATES; i++)
-                {
-                    instance.commands.Add(new CommandAction());
-                }
-
-                return instance;
-            }
-
-            public List<CommandAction> commands = new List<CommandAction>(5);
-
-            Dictionary<string,
-            TcpClient> tcpClients = new Dictionary<string,
-            TcpClient>();
-
-            [JsonProperty(PropertyName = "desiredStates")]
-            public int DesiredStates
-            {
-                get;
-                set;
-            }
-
-            #region State0
-
+            // JSON-bound properties for UI editor
             [JsonProperty(PropertyName = "commandPressed0")]
             public string CommandPressed0
             {
-                get
-                {
-                    return commands[0].CommandPressed;
-                }
-                set
-                {
-                    commands[0].CommandPressed = value;
-                }
+                get => commands[0].CommandPressed;
+                set => commands[0].CommandPressed = value;
             }
 
             [JsonProperty(PropertyName = "commandReleased0")]
             public string CommandReleased0
             {
-                get
-                {
-                    return commands[0].CommandReleased;
-                }
-                set
-                {
-                    commands[0].CommandReleased = value;
-                }
+                get => commands[0].CommandReleased;
+                set => commands[0].CommandReleased = value;
             }
-            #endregion
-
-            #region State1
 
             [JsonProperty(PropertyName = "commandPressed1")]
             public string CommandPressed1
             {
-                get
-                {
-                    return commands[1].CommandPressed;
-                }
-                set
-                {
-                    commands[1].CommandPressed = value;
-                }
+                get => commands[1].CommandPressed;
+                set => commands[1].CommandPressed = value;
             }
 
             [JsonProperty(PropertyName = "commandReleased1")]
             public string CommandReleased1
             {
-                get
-                {
-                    return commands[1].CommandReleased;
-                }
-                set
-                {
-                    commands[1].CommandReleased = value;
-                }
+                get => commands[1].CommandReleased;
+                set => commands[1].CommandReleased = value;
             }
-            #endregion
-
-            #region State2
 
             [JsonProperty(PropertyName = "commandPressed2")]
             public string CommandPressed2
             {
-                get
-                {
-                    return commands[2].CommandPressed;
-                }
-                set
-                {
-                    commands[2].CommandPressed = value;
-                }
+                get => commands[2].CommandPressed;
+                set => commands[2].CommandPressed = value;
             }
 
             [JsonProperty(PropertyName = "commandReleased2")]
             public string CommandReleased2
             {
-                get
-                {
-                    return commands[2].CommandReleased;
-                }
-                set
-                {
-                    commands[2].CommandReleased = value;
-                }
+                get => commands[2].CommandReleased;
+                set => commands[2].CommandReleased = value;
             }
-            #endregion
-
-            #region State3
 
             [JsonProperty(PropertyName = "commandPressed3")]
             public string CommandPressed3
             {
-                get
-                {
-                    return commands[3].CommandPressed;
-                }
-                set
-                {
-                    commands[3].CommandPressed = value;
-                }
+                get => commands[3].CommandPressed;
+                set => commands[3].CommandPressed = value;
             }
 
             [JsonProperty(PropertyName = "commandReleased3")]
             public string CommandReleased3
             {
-                get
-                {
-                    return commands[3].CommandReleased;
-                }
-                set
-                {
-                    commands[3].CommandReleased = value;
-                }
+                get => commands[3].CommandReleased;
+                set => commands[3].CommandReleased = value;
             }
-            #endregion
-
-            #region State4
 
             [JsonProperty(PropertyName = "commandPressed4")]
             public string CommandPressed4
             {
-                get
-                {
-                    return commands[4].CommandPressed;
-                }
-                set
-                {
-                    commands[4].CommandPressed = value;
-                }
+                get => commands[4].CommandPressed;
+                set => commands[4].CommandPressed = value;
             }
 
             [JsonProperty(PropertyName = "commandReleased4")]
             public string CommandReleased4
             {
-                get
-                {
-                    return commands[4].CommandReleased;
-                }
-                set
-                {
-                    commands[4].CommandReleased = value;
-                }
+                get => commands[4].CommandReleased;
+                set => commands[4].CommandReleased = value;
             }
-            #endregion
-
         }
 
         public class CommandAction
         {
-
-            public string CommandPressed
-            {
-                get;
-                set;
-            }
-
-            public string CommandReleased
-            {
-                get;
-                set;
-            }
-
+            public string CommandPressed { get; set; }
+            public string CommandReleased { get; set; }
         }
 
-        #region Private Members
-
-        private PluginSettings settings;
-
-        private ConnectionManager connectionManager;
-
-        #endregion
-        public PluginAction(SDConnection connection, InitialPayload payload) : base(connection, payload)
+        public PluginAction(SDConnection connection, InitialPayload payload)
+            : base(connection, payload)
         {
-            settings = payload.Settings?.ToObject<PluginSettings>() ?? PluginSettings.CreateDefaultSettings();
-
-            if (firstPluginLoad)
-            {
-                settings.StoredState = null;
-                _ = SaveSettings();
-                firstPluginLoad = false;
-
-                currentState = 0;
-                _ = SetStateAsync(0);
-            }
-            else
-            {
-                currentState = settings.StoredState ?? 0;
-                _ = SetStateAsync((uint)currentState);
-            }
+            settings =
+                payload.Settings?.ToObject<PluginSettings>()
+                ?? PluginSettings.CreateDefaultSettings();
+            currentState = settings.StoredState ?? 0;
+            _ = InitializeActionAsync();
 
             connectionManager = new ConnectionManager();
             connectionManager.InitializeClients();
@@ -253,45 +131,57 @@ namespace FXCommands
 
         public override void Dispose()
         {
-            settings.StoredState = null;
-            _ = SaveSettings();
-
             Logger.Instance.LogMessage(TracingLevel.INFO, "Destructor called");
             connectionManager.Dispose();
-            System.GC.Collect(); // Force garbage collection
         }
 
         public override async void KeyPressed(KeyPayload payload)
         {
             Logger.Instance.LogMessage(TracingLevel.INFO, "Key Pressed");
             if (!string.IsNullOrEmpty(CurrentCommandAction.CommandPressed))
-            {
                 await SendMessageAsync(CurrentCommandAction.CommandPressed);
-            }
         }
 
         public override async void KeyReleased(KeyPayload payload)
         {
             Logger.Instance.LogMessage(TracingLevel.INFO, "Key Released");
-
             if (!string.IsNullOrEmpty(CurrentCommandAction.CommandReleased))
-            {
                 await SendMessageAsync(CurrentCommandAction.CommandReleased);
-            }
 
-            currentState++;
-            if (currentState >= settings.DesiredStates)
-                currentState = 0;
-
+            currentState = (currentState + 1) % settings.DesiredStates;
             settings.StoredState = currentState;
             await SaveSettings();
-            await SetStateAsync((uint)currentState);
+            await UpdateUiAsync();
         }
 
-        private async Task SendMessageAsync(string message)
+        private async Task InitializeActionAsync()
         {
-            await Task.Run(() => connectionManager.SendMessage(message));
+            await UpdateUiAsync();
         }
+
+        private async Task UpdateUiAsync()
+        {
+            // Always set the state for built-in imagery
+            await Connection.SetStateAsync((uint)currentState);
+            // Update title
+            await Connection.SetTitleAsync(GetStateTitle(currentState));
+
+            // Only flash icon when there are multiple states
+            if (settings.DesiredStates > 1)
+            {
+                string iconFile = GetStateIcon(currentState);
+                if (!string.IsNullOrEmpty(iconFile))
+                    await Connection.SetImageAsync(iconFile);
+            }
+        }
+
+        private string GetStateTitle(int state) =>
+            settings.commands[state].CommandPressed ?? string.Empty;
+
+        private string GetStateIcon(int state) => $"state{state}.png";
+
+        private async Task SendMessageAsync(string message) =>
+            await Task.Run(() => connectionManager.SendMessage(message));
 
         public override void OnTick() { }
 
@@ -299,22 +189,11 @@ namespace FXCommands
         {
             Tools.AutoPopulateSettings(settings, payload.Settings);
             SaveSettings();
+            _ = UpdateUiAsync();
         }
 
         public override void ReceivedGlobalSettings(ReceivedGlobalSettingsPayload payload) { }
 
-        #region Private Methods
-
-        private Task SaveSettings()
-        {
-            return Connection.SetSettingsAsync(JObject.FromObject(settings));
-        }
-
-        private Task SetStateAsync(uint state)
-        {
-            return Connection.SetStateAsync(state);
-        }
-
-        #endregion
+        private Task SaveSettings() => Connection.SetSettingsAsync(JObject.FromObject(settings));
     }
 }
